1f9f014325e0b1ad6e21aa1423aa0bd92b6b9388
Alyson Ronnan Martins <alysonronnan@gmail.com>
2021-05-08 17:01:15 -0300

    :tada: Challenge Finished
    Lutei muito sozinho com erros como S.O. não conseguindo fazer os teste mais depois foi tranquilo.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import '../styles/tasklist.scss'
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare, FiEdit3 } from 'react-icons/fi'
 
 interface Task {
   id: number;
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+     if (!newTaskTitle) return
+
+     const taskNova = {
+       id: Math.random(),
+       title: newTaskTitle, 
+       isComplete: false,
+     }
+
+     setTasks(tasks=>[... tasks, taskNova])
+     setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const nTask = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task)
+
+    setTasks(nTask)
+
   }
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const nTask = tasks.filter(task => task.id != id)
+
+    setTasks(nTask)
   }
 
   return (
