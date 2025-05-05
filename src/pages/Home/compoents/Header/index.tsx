31d1d9b89de56bf2e968941adc25ad65e40aef2f
Alyson Ronnan Martins <devalyson@gmail.com>
2025-05-05 02:01:53 -0300

    fix: bug image svg

--- src/pages/Home/compoents/Header/index.tsx
+++ src/pages/Home/compoents/Header/index.tsx
@@ -1,10 +1,15 @@
+import ImgLogo from '../../../../assets/logo.svg'
+
 import { HeaderContainer, Logo, LogoContainer } from './styles'
 
 export function Header() {
 	return (
 		<HeaderContainer>
 			<LogoContainer>
-				<Logo src="src\assets\logo.svg" alt="Logo" />
+				<Logo
+					src={ImgLogo}
+					alt="Desenho de um foguete azul e a palavra TODO ao lado direto"
+				/>
 			</LogoContainer>
 		</HeaderContainer>
 	)
